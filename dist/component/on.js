881e4fdf8bf7f8c0f86c34d4d87ac78cce96f73e
kevinchau321 <kevinchau321@berkeley.edu>
2017-02-23 23:00:09 -0800

    add dist

---
+++ dist/component/on.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+  return _react2.default.createElement(
+    'svg',
+    { width: '37', height: '21', viewBox: '0 0 37 21' },
+    _react2.default.createElement(
+      'text',
+      { x: '0', y: '45%', fontWeight: '500', fontSize: '11', fill: '#34230B' },
+      'ON'
+    )
+  );
+};
